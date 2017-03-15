d6ec90956118302429f1f777e61f54c0acd4894f
Michael Ocheje <mocheje@c2gconsulting.com>
2017-03-15 13:47:46 +0100

    added export to other reports

--- main/app/client/templates/reports/netpay/netpay.js
+++ main/app/client/templates/reports/netpay/netpay.js
@@ -29,7 +29,56 @@ Template.NetPayReport.events({
           swal('Error', 'Please select Period', 'error');
       }
 
-   }
+   },
+    'click .excel': (e, tmpl) => {
+        event.preventDefault();
+        const month = $('[name="paymentPeriod.month"]').val();
+        const year = $('[name="paymentPeriod.year"]').val();
+        if(month && year) {
+            tmpl.$('.excel').text('Preparing... ');
+            tmpl.$('.excel').attr('disabled', true);
+
+
+            try {
+                let l = Ladda.create(tmpl.$('.excel')[0]);
+                l.start();
+            } catch(e) {
+                console.log(e);
+            }
+
+
+            let resetButton = function() {
+                // End button animation
+                try {
+                    let l = Ladda.create(tmpl.$('.excel')[0]);
+                    l.stop();
+                    l.remove();
+                } catch(e) {
+                    console.log(e);
+                }
+
+                tmpl.$('.excel').text(' Export to CSV');
+                // Add back glyphicon
+                $('.excel').prepend("<i class='glyphicon glyphicon-download'></i>");
+                tmpl.$('.excel').removeAttr('disabled');
+            };
+
+            const period = month + year;
+            Meteor.call('ExportNetPayResult', Session.get('context'), period, function(err, res){
+                if(res){
+                    //call the export fo
+                    BulkpayExplorer.exportAllData(res, "NetPaymentReport");
+                    resetButton()
+                } else {
+                    console.log(err);
+                    swal('No result found', 'Payroll Result not found for period', 'error');
+                }
+            });
+        } else {
+            swal('Error', 'Please select Period', 'error');
+        }
+
+    }
 });
 
 /*****************************************************************************/
