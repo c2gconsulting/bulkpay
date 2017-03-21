f8249c08524cf506836b68abb5cdb71ea58d5d9a
Efe Ariaroo <eariaroo@c2gconsulting.com>
2017-03-21 13:28:10 +0100

    Export of netpay reports now works

--- main/app/client/templates/reports/netpay/netpay.js
+++ main/app/client/templates/reports/netpay/netpay.js
@@ -16,19 +16,27 @@ Template.NetPayReport.events({
       const year = $('[name="paymentPeriod.year"]').val();
       if(month && year) {
           const period = month + year;
+
           Meteor.call('getnetPayResult', Session.get('context'), period, function(err, res){
               console.log(res);
               if(res && res.length){
                   console.log('logging response as ', res);
-                  tmpl.dict.set('result', res);
+                  tmpl.netPayReportResults.set(res);
               } else {
                   swal('No result found', 'Payroll Result not found for period', 'error');
+                  tmpl.netPayReportResults.set(null);
               }
           });
       } else {
           swal('Error', 'Please select Period', 'error');
       }
+   },
+   'click .Export': (e, tmpl) => {
+      let currentNetPayReportResults = tmpl.netPayReportResults.get();
+      if(currentNetPayReportResults) {
 
+        BulkpayExplorer.exportAllData(currentNetPayReportResults, `NetPay Report`);
+      }
    }
 });
 
@@ -51,7 +59,7 @@ Template.NetPayReport.helpers({
         return Core.years();
     },
     'result': () => {
-        return Template.instance().dict.get('result');
+        return Template.instance().netPayReportResults.get();
     }
 });
 
@@ -60,8 +68,7 @@ Template.NetPayReport.helpers({
 /*****************************************************************************/
 Template.NetPayReport.onCreated(function () {
     let self = this;
-    self.dict = new ReactiveDict();
-
+    self.netPayReportResults = new ReactiveVar();
 });
 
 Template.NetPayReport.onRendered(function () {
