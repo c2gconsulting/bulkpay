43b786830b0b24c4bdc82ba9ebded61705703086
Efe Ariaroo <eariaroo@c2gconsulting.com>
2017-04-19 18:50:47 +0100

    Now showing the list of procurements a user has created

--- main/app/client/templates/procurement_requisitions/procurement_requisition_index/procurement_requisition_index.js
+++ main/app/client/templates/procurement_requisitions/procurement_requisition_index/procurement_requisition_index.js
@@ -11,11 +11,18 @@ Template.ProcurementRequisitionIndex.events({
     }
 });
 
+
+Template.registerHelper('formatDate', function(date) {
+    return moment(date).format('DD-MM-YYYY');
+});
+
 /*****************************************************************************/
 /* ProcurementRequisitionIndex: Helpers */
 /*****************************************************************************/
 Template.ProcurementRequisitionIndex.helpers({
-
+    'procurementsICreated': function() {
+        return Template.instance().procurementsICreated();
+    }
 });
 
 /*****************************************************************************/
@@ -23,15 +30,51 @@ Template.ProcurementRequisitionIndex.helpers({
 /*****************************************************************************/
 Template.ProcurementRequisitionIndex.onCreated(function () {
     let self = this;
+    let businessUnitId = Session.get('context')
 
-    let businessUnitId = Session.get('context');
+    self.loaded = new ReactiveVar(0);
+    self.limit = new ReactiveVar(10);
+    //--
+    self.procurementsToApprove = new ReactiveVar()
 
-    self.subscribe('ProcurementRequisitionsICreated', businessUnitId)
-    self.subscribe('ProcurementRequisitionsToApprove', businessUnitId)
+    let procurementsToApproveSub = self.subscribe('ProcurementRequisitionsToApprove', businessUnitId)
 
     self.autorun(function() {
+        let limit = self.limit.get();
+        let sortBy = "createdAt";
+        let sortDirection =  -1;
+        let sort = {};
+        sort[sortBy] = sortDirection;
 
+        let procurementsCreatedSub = self.subscribe('ProcurementRequisitionsICreated', businessUnitId, limit, sort)
+        if (procurementsCreatedSub.ready()) {
+            self.loaded.set(10);
+        }
+        //--
+        if(procurementsToApproveSub.ready()) {
+            console.log(`ProcurementsSub is ready`)
+            let currentUser = Meteor.user()
+            if(currentUser.employeeProfile && currentUser.employeeProfile.employment) {
+                let currentUserPosition = currentUser.employeeProfile.employment.position
+
+                let procurementsToApprove = ProcurementRequisitions.find({supervisorPositionId: currentUserPosition}).fetch();
+                console.log(`procurementsToApprove: ${JSON.stringify(procurementsToApprove)}`)
+                self.procurementsToApprove.set(procurementsToApprove)
+            }
+        }
     })
+
+    self.procurementsICreated = function() {
+        let sortBy = "createdAt";
+        let sortDirection = -1;
+
+        let options = {};
+        options.sort = {};
+        options.sort[sortBy] = sortDirection;
+        options.limit = self.loaded.get();
+
+        return ProcurementRequisitions.find({createdBy: Meteor.userId()}, options);
+    }
 });
 
 Template.ProcurementRequisitionIndex.onRendered(function () {
