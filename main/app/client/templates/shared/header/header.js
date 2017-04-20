0c3ca11df35d210cf72782d71b40edf44b08a583
Efe Ariaroo <eariaroo@c2gconsulting.com>
2017-04-20 11:43:57 +0100

    Made Requisitions approval notification links pop up modal for approval

--- main/app/client/templates/shared/header/header.js
+++ main/app/client/templates/shared/header/header.js
@@ -1,3 +1,38 @@
+
+
+Template.header.events({
+    'click .requisitionRow': function(e, tmpl) {
+        e.preventDefault()
+        let requisitionId = e.currentTarget.getAttribute('data-RequisitionId')
+        console.log(`RequisitionId: ${requisitionId}`)
+
+        let invokeReason = {}
+        invokeReason.requisitionId = requisitionId
+        invokeReason.reason = 'approve'
+        invokeReason.approverId = Meteor.userId()
+
+        Modal.show('ProcurementRequisitionDetail', invokeReason)
+    }
+});
+
+
+Template.registerHelper('trimString', function(passedString, startstring, endstring) {
+    var theString = passedString.substring( startstring, endstring );
+    return new Handlebars.SafeString(theString)
+});
+
+Template.header.helpers({
+    'context': function(){
+        return Session.get('context');
+    },
+    'procurementsToApprove': function() {
+        return Template.instance().procurementsToApprove.get()
+    },
+    'currentUserId': function() {
+        return Meteor.userId();
+    }
+});
+
 Template.header.onCreated(function() {
     let self = this
 
@@ -30,14 +65,3 @@ Template.header.onRendered(function () {
 
 });
 
-Template.header.helpers({
-    'context': function(){
-        return Session.get('context');
-    },
-    'procurementsToApprove': function() {
-        return Template.instance().procurementsToApprove.get()
-    },
-    'currentUserId': function() {
-        return Meteor.userId();
-    }
-});
