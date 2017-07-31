8511cbbbd67815ec9d36f4ed945716717d456668
Efe Ariaroo <eariaroo@c2gconsulting.com>
2017-07-31 14:35:14 +0100

    disabled kadira

--- main/app/server/kadira.js
+++ main/app/server/kadira.js
@@ -1,5 +1,5 @@
-let appId = process.env.KADIRA_ID;
-let appSecret = process.env.KADIRA_SECRET;
-if (appId && appSecret) {
-	Kadira.connect(appId, appSecret);
-}
+// let appId = process.env.KADIRA_ID;
+// let appSecret = process.env.KADIRA_SECRET;
+// if (appId && appSecret) {
+// 	Kadira.connect(appId, appSecret);
+// }
