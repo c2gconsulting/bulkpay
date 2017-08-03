58a2301f699a45b5eec969413fc8f154af7eaeaf
Efe Ariaroo <eariaroo@c2gconsulting.com>
2017-08-03 18:11:05 +0100

    minor change to name shown in email notification

--- main/app/server/methods/procurementRequisition.js
+++ main/app/server/methods/procurementRequisition.js
@@ -9,7 +9,7 @@ let ProcurementRequisitonHelper = {
             SSR.compileTemplate("procurementRequisitionNotification", Assets.getText("emailTemplates/procurementRequisitionNotification.html"));
             Email.send({
                 to: "efeariaroo@gmail.com",
-                from: "BulkPay Team <eariaroo@c2gconsulting.com>",
+                from: "BulkPay™ Team <eariaroo@c2gconsulting.com>",
                 subject: "Procurement Requisition created!",
                 html: SSR.render("procurementRequisitionNotification", {
                     user: supervisorFullName,
