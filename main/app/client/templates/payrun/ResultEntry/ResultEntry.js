ec32a5c5decfce35681c6818920a127f05be4242
Michael Ukpong <mukpong@c2gconsulting.com>
2018-04-13 14:25:42 +0100

    Update to payslip ordering

--- main/app/client/templates/payrun/ResultEntry/ResultEntry.js
+++ main/app/client/templates/payrun/ResultEntry/ResultEntry.js
@@ -14,12 +14,82 @@ Template.ResultEntry.events({
 
         let payLoadForPayslip = {
             payslip: tmpl.data.payslip,
-            payslipWithCurrencyDelineation: tmpl.data.payslipWithCurrencyDelineation, 
+            payslipWithCurrencyDelineation: tmpl.data.payslipWithCurrencyDelineation,
             displayAllPaymentsUnconditionally: true
         }
+        //console.log("payslip info ");
+        //console.log(payLoadForPayslip.payslip);
+
+        let findObjectByKey = function (array, key, value) {
+            for (var i = 0; i < array.length; i++) {
+                if (array[i][key] === value) {
+                    return array[i];
+                }
+            }
+            return null;
+        };
+
+        const paygrade = PayGrades.findOne({_id: payLoadForPayslip.payslip.employee.gradeId})
+
+        if(paygrade) {
+            //console.log(paygrade.payTypePositionIds);
+
+            payLoadForPayslip.payslip.benefit.sort(function(a, b){
+                let aa = findObjectByKey(paygrade.payTypePositionIds, "paytype", a.payTypeId);
+                let aaPosition = 99;
+                if (aa){
+                    aaPosition = parseInt(aa.paySlipPositionId);
+                }
+
+                let bb = findObjectByKey(paygrade.payTypePositionIds, "paytype", b.payTypeId);
+
+                let bbPosition = 99;
+                if (bb){
+                    bbPosition = parseInt(bb.paySlipPositionId);
+                }
+
+                return aaPosition - bbPosition;
+            });
+
+            payLoadForPayslip.payslip.deduction.sort(function(a, b){
+                let aa = findObjectByKey(paygrade.payTypePositionIds, "paytype", a.payTypeId);
+                let aaPosition = 99;
+                if (aa){
+                    aaPosition = parseInt(aa.paySlipPositionId);
+                }
+
+                let bb = findObjectByKey(paygrade.payTypePositionIds, "paytype", b.payTypeId);
+
+                let bbPosition = 99;
+                if (bb){
+                    bbPosition = parseInt(bb.paySlipPositionId);
+                }
+
+                return aaPosition - bbPosition;
+            });
+
+            payLoadForPayslip.payslip.others.sort(function(a, b){
+                let aa = findObjectByKey(paygrade.payTypePositionIds, "paytype", a.payTypeId);
+                let aaPosition = 99;
+                if (aa){
+                    aaPosition = parseInt(aa.paySlipPositionId);
+                }
+
+                let bb = findObjectByKey(paygrade.payTypePositionIds, "paytype", b.payTypeId);
+
+                let bbPosition = 99;
+                if (bb){
+                    bbPosition = parseInt(bb.paySlipPositionId);
+                }
+
+                return aaPosition - bbPosition;
+            });
+
+        }
+
         Session.set('currentSelectedPaySlip', payLoadForPayslip)
         $("#payrunResultsTab").removeClass( "active" )
-        $("#selectedPaySlipTab").addClass( "active" )        
+        $("#selectedPaySlipTab").addClass( "active" )
 
         $(".payrunResults").removeClass( "active" )
         $(".selectedPaySlip").addClass( "active" )
