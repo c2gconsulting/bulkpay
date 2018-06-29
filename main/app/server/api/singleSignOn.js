2d3cdcd2c50a79ba122ff1ad74390a101ae2c143
Zekeri Zekkerriyya <zzekeri@c2gconsulting.com>
2018-06-29 09:27:44 +0100

    changes

--- main/app/server/api/singleSignOn.js
+++ main/app/server/api/singleSignOn.js
@@ -1,27 +1,27 @@
-Meteor.startup(function() {
-    ServiceConfiguration.configurations.upsert(
-        { service: "adfsoauth" },
-        {
-            $set: {
-                clientId: "1db8ab0d-9d08-4a89-95ca-f69daa5f701d",
-                loginStyle: "popup",
-                secret: "none",
-                publicCertPath : "/hdc01/c$/Certificates Export/Certs/Public key/hdc01.ad.hsdf.org.ng-2018-06-25-064557.cer",
-                resource : "sandbox.bulkpay.co/business/pdgypekWZKA3yTgEa",
-                profileNameField : "commonname",
-                oauthAdfsUrl : "https://hdc01.ad.hsdf.org.ng/adfs/oauth2",
-                redirectUrl: "http://sandbox.bulkpay.co/_oauth/adfsoauth"
-            }
-        }
-    );
-});
-if (Meteor.isClient) {
-    Meteor.startup(function () {
-       if (Meteor.user()) {
-           console.log('User logged in!');
-       } else {
-           console.log('User logged out!');
-           Meteor.loginWithAdfsoauth(); //Auto login using ADFS Oauth
-       }
-    });
-}
+// Meteor.startup(function() {
+//     ServiceConfiguration.configurations.upsert(
+//         { service: "adfsoauth" },
+//         {
+//             $set: {
+//                 clientId: "1db8ab0d-9d08-4a89-95ca-f69daa5f701d",
+//                 loginStyle: "popup",
+//                 secret: "none",
+//                 publicCertPath : "/hdc01/c$/Certificates Export/Certs/Public key/hdc01.ad.hsdf.org.ng-2018-06-25-064557.cer",
+//                 resource : "sandbox.bulkpay.co/business/pdgypekWZKA3yTgEa",
+//                 profileNameField : "commonname",
+//                 oauthAdfsUrl : "https://hdc01.ad.hsdf.org.ng/adfs/oauth2",
+//                 redirectUrl: "http://sandbox.bulkpay.co/_oauth/adfsoauth"
+//             }
+//         }
+//     );
+// });
+// if (Meteor.isClient) {
+//     Meteor.startup(function () {
+//        if (Meteor.user()) {
+//            console.log('User logged in!');
+//        } else {
+//            console.log('User logged out!');
+//            Meteor.loginWithAdfsoauth(); //Auto login using ADFS Oauth
+//        }
+//     });
+// }
