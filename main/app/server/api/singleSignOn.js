4745e819e24ad6afbc9648688532e88625c152f8
Zekeri Zekkerriyya <zzekeri@c2gconsulting.com>
2018-07-04 15:59:32 +0100

    SSO

--- main/app/server/api/singleSignOn.js
+++ main/app/server/api/singleSignOn.js
@@ -1,15 +1,15 @@
-import { HTTP } from 'meteor/http'
-HTTP.call( 'GET', 'https://hdc01.ad.hsdf.org.ng/adfs/oauth2/authorize?response_type=code&client_id=1db8ab0d-9d08-4a89-95ca-f69daa5f701d&redirect_uri=http://sandbox.bulkpay.co/_oauth/adfsoauth&resource=sandbox.bulkpay.co/business/pdgypekWZKA3yTgEa', {
-  // params: {
-  //   "response_type":"code",
-  //   "client_id":"1db8ab0d-9d08-4a89-95ca-f69daa5f701d"
-  //   "redirect_uri":"http://sandbox.bulkpay.co/_oauth/adfsoauth"
-  //   "resource":"sandbox.bulkpay.co/business/pdgypekWZKA3yTgEa"
-  //
-  // }
-}, function( error, response ) {
-  if ( error ) {
-    console.log( error );
-  } else {
-    console.log( response );  }
-});
+// import { HTTP } from 'meteor/http'
+// HTTP.call( 'GET', 'https://hdc01.ad.hsdf.org.ng/adfs/oauth2/authorize?response_type=code&client_id=1db8ab0d-9d08-4a89-95ca-f69daa5f701d&redirect_uri=http://sandbox.bulkpay.co/_oauth/adfsoauth&resource=sandbox.bulkpay.co/business/pdgypekWZKA3yTgEa', {
+//   // params: {
+//   //   "response_type":"code",
+//   //   "client_id":"1db8ab0d-9d08-4a89-95ca-f69daa5f701d"
+//   //   "redirect_uri":"http://sandbox.bulkpay.co/_oauth/adfsoauth"
+//   //   "resource":"sandbox.bulkpay.co/business/pdgypekWZKA3yTgEa"
+//   //
+//   // }
+// }, function( error, response ) {
+//   if ( error ) {
+//     console.log( error );
+//   } else {
+//     console.log( response );  }
+// });
