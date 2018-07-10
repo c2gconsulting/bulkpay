0fe4aa212e4d7c7376a3d603257a32a75027ae1a
Zekeri Zekkerriyya <zzekeri@c2gconsulting.com>
2018-07-10 11:38:31 +0100

    getting the authorization code from http

--- main/app/server/api/singleSignOn.js
+++ main/app/server/api/singleSignOn.js
@@ -1,17 +1,30 @@
 import { HTTP } from 'meteor/http'
-HTTP.call( 'GET',
-'https://hdc01.ad.hsdf.org.ng/adfs/oauth2/authorize',
- {
-  params: {
-    "response_type":"code",
-    "client_id":"1db8ab0d-9d08-4a89-95ca-f69daa5f701d",
-    "redirect_uri":"http://sandbox.bulkpay.co/_oauth/adfsoauth",
-    "resource":"sandbox.bulkpay.co/business/pdgypekWZKA3yTgEa"
+// var options = { host: 'hdc01.ad.hsdf.org.ng', 
+//                 path: '/adfs/oauth2/authorize',
+//                 method:'GET' };
+// HTTP.call(options,
+//  {
+//   params: {
+//     "response_type":"code",
+//     "client_id":"1db8ab0d-9d08-4a89-95ca-f69daa5f701d",
+//     "redirect_uri":"http://sandbox.bulkpay.co/_oauth/adfsoauth",
+//     "resource":"sandbox.bulkpay.co/business/pdgypekWZKA3yTgEa"
 
-  }
-}, function( error, response ) {
-  if ( error ) {
-    console.log( error );
-  } else {
-    console.log( response );  }
-});
+//   }
+// }, function( error, response ) {
+//   if ( error ) {
+//     console.log( error );
+//   } else {
+//     console.log( response );  }
+// });
+var response = HTTP.call( 'GET', 'hdc01.ad.hsdf.org.ng/adfs/oauth2/authorize', {
+  params: {
+        "response_type":"code",
+        "client_id":"1db8ab0d-9d08-4a89-95ca-f69daa5f701d",
+        "redirect_uri":"http://sandbox.bulkpay.co/_oauth/adfsoauth",
+        "resource":"sandbox.bulkpay.co/business/pdgypekWZKA3yTgEa"
+    
+      }
+} );
+console.log("response is:" );
+console.log(response );
